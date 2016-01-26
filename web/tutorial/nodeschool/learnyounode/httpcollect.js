d4cf043ce7c0460b1cfe05b310aa42abc7790367
LianyuMa <LianyuMa@users.noreply.github.com>
2016-01-26 14:56:09 -0500

    HTTP COLLECT approach I

--- web/tutorial/nodeschool/learnyounode/httpcollect.js
+++ web/tutorial/nodeschool/learnyounode/httpcollect.js
@@ -5,7 +5,7 @@ var content = '';
 http.get(url, function(response) {
   response.setEncoding('utf8');
   response.on('error', function(err) {
-    console.log(err);
+    console.error(err);
   });
   response.on('data', function(data) {
     content += data;
