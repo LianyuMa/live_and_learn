07b6d9bc7d7f95953be795ff37f048db0cfacc7b
LianyuMa <LianyuMa@users.noreply.github.com>
2016-02-21 17:16:09 -0500

    Get Your Level On!

---
+++ web/tutorial/nodeschool/levelmeupscotty/getyourlevelon.js
@@ -0,0 +1,8 @@
+var level = require('level');
+var db = level(process.argv[2]);
+
+db.get(process.argv[3], function (err, value) {
+  if (err) return console.log('Ooops!', err);
+
+  console.log(value);
+});
