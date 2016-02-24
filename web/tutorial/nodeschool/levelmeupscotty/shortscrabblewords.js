38e5a27b5a06d6756916eed03110d21ca01b675b
LianyuMa <LianyuMa@users.noreply.github.com>
2016-02-23 20:10:04 -0500

    Short Scrabble Words

---
+++ web/tutorial/nodeschool/levelmeupscotty/shortscrabblewords.js
@@ -0,0 +1,23 @@
+module.exports.init = function (db, words, callback) {
+  var batchCommands = words.map(function (word) {
+    var key = word.length + '!' + word;
+    return { type: 'put', key: key, value: word };
+  });
+
+  db.batch(batchCommands, callback);
+};
+
+module.exports.query = function (db, word, callback) {
+  var key = word.length + '!' + word.replace(/\*/g, '');
+  var arr = [];
+  var readStream = db.createReadStream({ start: key, end: key + '\xff' });
+  readStream.on('data', function (data) {
+    arr.push(data.value);
+  });
+  readStream.on('error', function (err) {
+    callback(err);
+  });
+  readStream.on('end', function () {
+    callback(null, arr);
+  });
+};
