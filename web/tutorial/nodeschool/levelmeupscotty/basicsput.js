5d9bc2e94572888f762679b401e4915ce8dc643c
LianyuMa <LianyuMa@users.noreply.github.com>
2016-02-24 20:29:46 -0500

    Basics: Put

--- web/tutorial/nodeschool/levelmeupscotty/basicsput.js
+++ web/tutorial/nodeschool/levelmeupscotty/basicsput.js
@@ -1,14 +1,20 @@
-var level = require('level');
-var db = level(process.argv[2]);
-var obj = JSON.parse(process.argv[3]);
+'use strict';
 
-var propNames = Object.getOwnPropertyNames(obj);
+var level     = require('level'),
+    db        = level(process.argv[2]),
+    obj       = JSON.parse(process.argv[3]),
+    key
+    ;
 
-for (var key in obj) {
-  db.put(key, obj[key], function (err) {
-    if (err) return console.error('Error in put():', err);
-    console.error('put foo = bar');
-  });
+function callback (err) {
+  if (err) {return console.error('Error in put():', err);}
+  return console.error('put foo = bar');
+}
+
+for (key in obj) {
+  if (obj.hasOwnProperty(key)) {
+    db.put(key, obj[key], callback ());
+  }  
 }
 
 db.close();
