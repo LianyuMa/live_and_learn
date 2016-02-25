aa1e9bdee966bb0a175e9ebbd9d5895de1d1562f
LianyuMa <LianyuMa@users.noreply.github.com>
2016-02-24 19:02:07 -0500

    Basic: Strict

--- web/tutorial/nodeschool/levelmeupscotty/basicsbatch.js
+++ web/tutorial/nodeschool/levelmeupscotty/basicsbatch.js
@@ -1,22 +1,26 @@
-var level = require('level');
-var db = level(process.argv[2]);
-var fs = require('fs');
-var filePath = process.argv[3];
+"use strict";
 
-fs.readFile(filePath, {encoding: 'utf8'}, function(err, data) {
-  if (err) return err;
-  var lines = data.split('\n');
-  var batchCommands = [];
+var level    = require('level'),
+    db       = level(process.argv[2]),
+    fs       = require('fs'),
+    filePath = process.argv[3]
+    ;
+
+fs.readFile(filePath, {encoding: 'utf8'}, function (err, data) {
+  if (err) { return err; }
+  var lines = data.split('\n'),
+      batchCommands = []
+      ;
   lines.forEach(function(line) {
     var parts = line.split(',');
-    if (parts[0] == 'put') {
+    if (parts[0] === 'put') {
       batchCommands.push({
         type : 'put',
         key  : parts[1],
         value: parts[2]
       });
     }
-    else if (parts[0] == 'del') {
+    else if (parts[0] === 'del') {
       batchCommands.push({
         type: 'del',
         key : parts[1]
