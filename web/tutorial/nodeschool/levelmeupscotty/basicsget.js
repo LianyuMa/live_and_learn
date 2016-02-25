5307569715a6b5e51f272d05b47438384c686fd8
LianyuMa <LianyuMa@users.noreply.github.com>
2016-02-24 20:02:04 -0500

    Basics: Get

--- web/tutorial/nodeschool/levelmeupscotty/basicsget.js
+++ web/tutorial/nodeschool/levelmeupscotty/basicsget.js
@@ -1,18 +1,45 @@
-var level = require('level');
-var db = level(process.argv[2]);
+"use strict";
+// JSLint: Don't make functions within a loop
+// var 
+//     i, a,
+//     level = require('level'),
+//     db    = level(process.argv[2])
+//     ;
 
-for (var i = 0; i < 101; i++) {
-  var a = i;
-  (function(a){
-    db.get('key' + a, function (err, value) {
-      if (err) {
-        if (err.notFound) {
-          return;
-        }
-        return err;
+// for (i = 0; i < 101; i++) {
+//   a = i;
+//   (function(a){
+//     db.get('key' + a, function (err, value) {
+//       if (err) {
+//         if (err.notFound) {
+//           return;
+//         }
+//         return err;
+//       }
+
+//       console.log('key' + a + '=' + value);
+//     });
+//   }(a)); 
+// }
+
+var level = require('level'),
+    db    = level(process.argv[2]);
+
+function fetchNext (i) {
+  var key = 'key' + i;
+  db.get(key, function (err, data) {
+    if (err) {
+      if (!err.notFound) {
+        throw err;
       }
+    } else {
+      console.log(key + '=' + data);
+    }
 
-      console.log('key' + a + '=' + value);
-    });
-  })(a); 
-};
+    if (i < 100) {
+      fetchNext(i + 1);
+    }
+  });
+}
+
+fetchNext(0);
