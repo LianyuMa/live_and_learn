29f16699aa620664268c5b04c376b23bb9101b34
LianyuMa <LianyuMa@users.noreply.github.com>
2016-03-03 00:45:07 -0500

    HELPING

---
+++ web/tutorial/nodeschool/makemehapi/helping.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var
+  Hapi       = require('hapi'),
+  server     = new Hapi.Server(),
+  path       = require('path'),
+  Vision     = require('vision'),
+  handlebars = require('handlebars'),
+  portNum    = process.argv[2]
+  ;
+
+server.connection({
+  host: 'localhost',
+  port: Number(portNum || 8080),
+});
+
+server.register(Vision, function (err) {
+  if (err) {throw err;}
+});
+
+server.route({
+  path   : '/',
+  method : 'GET',
+  handler: {
+    view: 'helping_index.html'
+  }
+});
+
+server.views({
+  engines    : {
+    html: handlebars
+  },
+  path       : path.join(__dirname, 'templates'),
+  helpersPath: 'helpers'
+});
+
+server.start(function (err) {
+  if (err) {throw err;}
+  console.log('Server running at: ', server.info.uri);
+});
