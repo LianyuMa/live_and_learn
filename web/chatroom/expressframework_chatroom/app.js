e7656821b85d05cade20d2bcf635e70c385616bf
LianyuMa <LianyuMa@users.noreply.github.com>
2016-03-04 21:58:22 -0500

    mongodb connection

--- web/chatroom/expressframework_chatroom/app.js
+++ web/chatroom/expressframework_chatroom/app.js
@@ -13,6 +13,17 @@ var users = require('./routes/users');
 
 var app = express();
 
+//mongodb connection
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
 //Socket.io
 var io = socket_io();
 app.io = io;
