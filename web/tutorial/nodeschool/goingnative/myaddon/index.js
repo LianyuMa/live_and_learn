3714d835f6ccf28a5409aad797b8aac90b34d883
LianyuMa <LianyuMa@users.noreply.github.com>
2016-03-25 19:00:43 -0400

    IT'S A TWO-WAY STREET

--- web/tutorial/nodeschool/goingnative/myaddon/index.js
+++ web/tutorial/nodeschool/goingnative/myaddon/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var bindings = require('bindings'),
-    myaddon  = bindings('myaddon')
-    ;
+// var bindings = require('bindings'),
+//     myaddon  = bindings('myaddon')
+//     ;
 
-myaddon.print(process.argv[2]);
+var addon = require('bindings')('myaddon');
+
+// myaddon.print(process.argv[2]);
+console.log(addon.length(process.argv[2]));
