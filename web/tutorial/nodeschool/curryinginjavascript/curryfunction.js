ae795d8e8ef6977670d665ae8d53822e8c3dd739
LianyuMa <LianyuMa@users.noreply.github.com>
2016-04-13 22:37:38 -0400

    Curry Function

--- web/tutorial/nodeschool/curryinginjavascript/curryfunction.js
+++ web/tutorial/nodeschool/curryinginjavascript/curryfunction.js
@@ -1,17 +1,32 @@
-function convertToCurry (fx) {
-	const arity = fx.length;
+function convertToCurry(fx) {
+  const arity = fx.length;
 
-	return function f1 () {
-		const args = Array.prototype.slice.call(arguments, 0);
-		if (args.length >= arity) {
-			return fx.apply(null, args);
-		} else {
-			return function f2 () {
-				const args2 = Array.prototype.slice.call(arguments, 0);
-				return f1.apply(null, args.concat(args2));
-			}
-		}
-	};
+  return function f1() {
+    const args = Array.prototype.slice.call(arguments, 0);
+    if (args.length >= arity) {
+      return fx.apply(null, args);
+    }
+    return function f2() {
+      const args2 = Array.prototype.slice.call(arguments, 0);
+      return f1.apply(null, args.concat(args2));
+    };
+  };
 }
+
+// ES6 Rest parameters
+// function convertToCurry(fx) {
+//   const arity = fx.length;
+
+//   return function f1(...theArgs) {
+//     const theArgs = Array.prototype.slice.call(arguments, 0);
+//     if (theArgs.length >= arity) {
+//       return fx.apply(null, theArgs);
+//     }
+//     return function f2(...theArgs2) {
+//       const theArgs2 = Array.prototype.slice.call(arguments, 0);
+//       return f1.apply(null, theArgs.concat(theArgs2));
+//     };
+//   };
+// }
 
 module.exports = convertToCurry;
