989026af7531610dc7750bd5256f847a7bb636e5
LianyuMa <LianyuMa@users.noreply.github.com>
2016-04-22 23:15:06 -0400

    Sort Me

--- web/tutorial/nodeschool/lololodash/sortme.js
+++ web/tutorial/nodeschool/lololodash/sortme.js
@@ -1,6 +1,4 @@
 const _ = require('lodash');
-const sortByQuantity = (collection) => {
-  return >.sortBy(collection, (item) => -item.quantity);
-};
+const sortByQuantity = (collection) => _.sortBy(collection, (item) => -item.quantity);
 
 module.exports = sortByQuantity;
