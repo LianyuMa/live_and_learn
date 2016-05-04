cb60b1562c0bbfa704ad2a16f085d10e7f812425
LianyuMa <LianyuMa@users.noreply.github.com>
2016-05-03 22:32:26 -0400

    add latency for target server

--- web/proxyserver/nodeserver/server.js
+++ web/proxyserver/nodeserver/server.js
@@ -1,13 +1,16 @@
-//
-// Basic stand-alone proxy server
-//
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-httpProxy.createProxyServer({ target: 'http://localhost:9000' }).listen(8000);
+const proxy = httpProxy.createProxyServer();
+
+http.createServer((req, res) => {
+  setTimeout(() => {
+    proxy.web(req, res, { target: 'http://localhost:9008' });
+  }, 5000);
+}).listen(8008);
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write(`request successfully proxied!\n${JSON.stringify(req.headers, true, 2)}`);
+  res.write(`request successfully proxied to: ${req.url}\n${JSON.stringify(req.headers, true, 2)}`);
   res.end();
-}).listen(9000);
+}).listen(9008);
