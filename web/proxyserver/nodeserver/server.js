0fdb5f68a884da19b3fb753981852a7d45149f72
LianyuMa <LianyuMa@users.noreply.github.com>
2016-05-03 21:40:52 -0400

    basic stand-alone proxy server

---
+++ web/proxyserver/nodeserver/server.js
@@ -0,0 +1,13 @@
+//
+// Basic stand-alone proxy server
+//
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+httpProxy.createProxyServer({ target: 'http://localhost:9000' }).listen(8000);
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write(`request successfully proxied!\n${JSON.stringify(req.headers, true, 2)}`);
+  res.end();
+}).listen(9000);
