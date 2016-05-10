f8cb8a57c58ab9d1abe9c5d6fb51b59e6462953f
LianyuMa <LianyuMa@users.noreply.github.com>
2016-05-09 22:50:44 -0400

    phantomjs pageloading

---
+++ web/phantomjs/pageloading.js
@@ -0,0 +1,8 @@
+const page = require('webpage').create();
+page.open('http://example.com', function (status) {
+  console.log('status: ' + status);
+  if (status === 'success') {
+    page.render('example.png');
+  }
+  phantom.exit();
+});
