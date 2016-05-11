f8aefdea5ab82ec73f6619cb8db55c1744684a14
root <LianyuMa@users.noreply.github.com>
2016-05-10 22:44:19 -0400

    get the title of webpage

---
+++ web/phantomjs/getpagetitle.js
@@ -0,0 +1,21 @@
+var page = require('webpage').create();
+var system = require('system');
+var url;
+
+if (system.args.length === 1) {
+  console.log('Usage: loadspeed.js <some URL>');
+  phantom.exit();
+}
+
+url = system.args[1];
+
+page.onConsoleMessage = function (msg) {
+  console.log('Page title is ' + msg);
+};
+
+page.open(url, function (status) {
+  var title = page.evaluate(function () {
+    console.log(document.title);;
+  });
+  phantom.exit();
+});
