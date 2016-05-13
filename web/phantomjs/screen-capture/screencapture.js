0329deb4eebb77fce85e4e07bc31fb8e6febfafb
root <LianyuMa@users.noreply.github.com>
2016-05-12 23:38:25 -0400

    rasterize

--- web/phantomjs/screen-capture/screencapture.js
+++ web/phantomjs/screen-capture/screencapture.js
@@ -1,5 +1,7 @@
+'use strict';
+
 var page = require('webpage').create();
 page.open('http://github.com/', function () {
-  page.render('github.png'); //Beside PNG format, PhantomJS supports JPEG, GIF, and PDF
+  page.render('github.png'); // Beside PNG format, PhantomJS supports JPEG, GIF, and PDF
   phantom.exit();
 });
