896b8eef1a88173aa65cd938ae40a2ba670f38d0
root <LianyuMa@users.noreply.github.com>
2016-05-23 22:46:08 -0400

    electron

---
+++ web/electron/quick-start/main.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+// Module to control application life.
+const {app} = electron;
+// Module to create native browser window.
+const {BrowserWindow} = electron;
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600});
+  win.loadURL(`file://${__dirname}/index.html`);
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
