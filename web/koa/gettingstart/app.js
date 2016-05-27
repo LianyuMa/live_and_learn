c81a40ff75cbea9b617c77716ec9ac7c5ec0db5b
root <LianyuMa@users.noreply.github.com>
2016-05-26 23:46:52 -0400

    throw exception

--- web/koa/gettingstart/app.js
+++ web/koa/gettingstart/app.js
@@ -1,21 +1,29 @@
 const koa = require('koa');
 const app = koa();
 
-app.use(function* (next) {
+app.use(function* responseTime(next) {
   const start = new Date;
   yield next;
   const ms = new Date - start;
   this.set('X-Response-Time', `${ms}ms`);
 });
 
-app.use(function* (next) {
+app.use(function* logger(next) {
   const start = new Date;
   yield next;
   const ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
+  console.log('%s %s %s - %sms', this.method, this.originalUrl, this.status, ms);
 });
 
-app.use(function* () {
+app.use(function* contentLength(next) {
+  yield next;
+  if (!this.body) return;
+  this.set('Content-Length', this.body.length);
+});
+
+app.use(function* body(next) {
+  yield next;
+  if (this.path !== '/') return;
   this.body = 'Hello world!';
 });
 
