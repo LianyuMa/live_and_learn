ac31ba8715978e287100fea7cf6731b2f79e2a86
root <LianyuMa@users.noreply.github.com>
2016-05-26 23:11:07 -0400

    cascading use yield

--- web/koa/gettingstart/app.js
+++ web/koa/gettingstart/app.js
@@ -1,7 +1,21 @@
 const koa = require('koa');
 const app = koa();
 
-app.use(function *() {
+app.use(function* (next) {
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  this.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(function* (next) {
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(function* () {
   this.body = 'Hello world!';
 });
 
