d0a869d6ccf205012755b7494c5e9d30e0f658e8
root <LianyuMa@users.noreply.github.com>
2016-06-05 17:56:22 -0400

    improve test

--- web/koa/gettingstart/negotiation/test.js
+++ web/koa/gettingstart/negotiation/test.js
@@ -23,4 +23,37 @@ describe('negotiation', () => {
         .expect('<name>cyclops</name>', done);
     });
   });
+
+  describe('html', () => {
+    it('should respond with html', (done) => {
+      request
+        .get('/cyclops')
+        .set('Accept', 'text/html')
+        .expect(200)
+        .expect('Content-Type', /html/)
+        .expect('<h1>cyclops</h1>', done);
+    });
+  });
+
+  describe('text', () => {
+    it('should respond with html', (done) => {
+      request
+        .get('/cyclops')
+        .set('Accept', 'text/plain')
+        .expect(200)
+        .expect('Content-Type', /plain/)
+        .expect('cyclops', done);
+    });
+  });
+
+  describe('*/*', () => {
+    it('should give precedence to the first accepted type', (done) => {
+      request
+        .get('/cyclops')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect('{"name":"cyclops","formerly":"Scott Summers"}', done);
+    });
+  });
 });
