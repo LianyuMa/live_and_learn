6f12fced5aff4321ad3795895c3279c016fd3e7f
root <LianyuMa@users.noreply.github.com>
2016-06-12 20:46:50 -0400

    passed by eslint

--- web/webpack/react-es6/src/App.js
+++ web/webpack/react-es6/src/App.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Paragraph from './Paragraph';
 
 // const App = () => {
 //   return '<h1>Woop</h1>';
 // };
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="my-app">
-        <h1>This is React!!!</h1>
-        <Paragraph text="First Paragraph" />
-        <Paragraph text="Second Paragraph" />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="my-app">
+      <h1>This is React!!!</h1>
+      <Paragraph text="First Paragraph" />
+      <Paragraph text="Second Paragraph" />
+    </div>
+  );
 }
 
 // module.exports = App;
--- web/webpack/react-es6/src/Paragraph.js
+++ web/webpack/react-es6/src/Paragraph.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Paragraph extends Component {
-  render() {
-    return (
-      <p>{this.props.text}</p>
-    );
-  }
+export default function Paragraph({ text }) {
+  return (
+    <p>{text}</p>
+  );
 }
+
+Paragraph.propTypes = {
+  text: React.PropTypes.string.isRequired,
+};
