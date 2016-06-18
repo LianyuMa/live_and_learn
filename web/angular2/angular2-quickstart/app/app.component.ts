3d8b76694358d537efd8ad4ffe7b36cbed6c7cd6
root <LianyuMa@users.noreply.github.com>
2016-06-17 21:36:59 -0400

    update new edtion

--- web/angular2/angular2-quickstart/app/app.component.ts
+++ web/angular2/angular2-quickstart/app/app.component.ts
@@ -1,7 +1,6 @@
-///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
-import {Component} from 'angular2/core';
+import { Component } from '@angular/core';
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
