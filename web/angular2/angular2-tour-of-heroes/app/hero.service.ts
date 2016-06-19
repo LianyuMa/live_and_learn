8a74381ac8374b6983434361a4d791fb33c92063
root <LianyuMa@users.noreply.github.com>
2016-06-19 14:49:42 -0400

    Add rouiting and dashboard

--- web/angular2/angular2-tour-of-heroes/app/hero.service.ts
+++ web/angular2/angular2-tour-of-heroes/app/hero.service.ts
@@ -3,13 +3,13 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  // getHeroes() {
-  //   return Promise.resolve(HEROES);
-  // }
-
-  getHeroesSlowly() {
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
-    );
+  getHeroes() {
+    return Promise.resolve(HEROES);
   }
+
+  // getHeroesSlowly() {
+  //   return new Promise<Hero[]>(resolve =>
+  //     setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+  //   );
+  // }
 }
