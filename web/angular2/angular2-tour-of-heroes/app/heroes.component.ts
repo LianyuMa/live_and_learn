7260462408f783c566644a77b7f8f33c2baeac85
root <LianyuMa@users.noreply.github.com>
2016-06-19 15:59:40 -0400

    Dashboard Top Heroes

--- web/angular2/angular2-tour-of-heroes/app/heroes.component.ts
+++ web/angular2/angular2-tour-of-heroes/app/heroes.component.ts
@@ -6,7 +6,6 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-heroes',
   template: `
-    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
@@ -68,7 +67,7 @@ import { HeroService } from './hero.service';
 })
 
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
+  // title = 'Tour of Heroes';
   // hero = 'windstorm';
   // hero: Hero = {
   //   id: 1,
