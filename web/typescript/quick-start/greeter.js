6f90ea46291865367324636b4108960d271c4f94
LianyuMa <LianyuMa@users.noreply.github.com>
2016-06-27 23:24:23 -0400

    Classes

--- web/typescript/quick-start/greeter.js
+++ web/typescript/quick-start/greeter.js
@@ -1,5 +1,16 @@
+// Argument of type 'number[]' is not assignable to parameter of type 'string'.
+// function greeter(person: string) {
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "Jane", lastName: "User" };
+var user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
--- web/typescript/quick-start/greeter.ts
+++ web/typescript/quick-start/greeter.ts
@@ -1,5 +1,12 @@
 // Argument of type 'number[]' is not assignable to parameter of type 'string'.
 // function greeter(person: string) {
+class Student {
+  fullName: string;
+  constructor(public firstName, public middleInitial, public lastName) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
 interface Person {
   firstName: string;
   lastName: string;
@@ -9,6 +16,6 @@ function greeter(person: Person) {
   return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-var user = { firstName: "Jane", lastName: "User" };
+var user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
