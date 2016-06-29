58649c6b32d59ac6f956754ac96b39ed45579293
LianyuMa <LianyuMa@users.noreply.github.com>
2016-06-28 23:31:30 -0400

    tsc code in ./src

---
+++ web/typescript/react-webpack/src/components/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "reacrt-dom";
+
+import { Hello } from "./component/Hello";
+
+ReactDOM.render(
+  <Hello compiler="TypeScript" framework="React" />
+  document.getElementById("example");
+);
