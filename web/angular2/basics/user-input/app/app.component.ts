7faab04f6e4f33b991a3cf46f8499ca89269030f
LianyuMa <LianyuMa@users.noreply.github.com>
2016-06-30 17:30:37 -0400

    Little Tour of Heroes

--- web/angular2/basics/user-input/app/app.component.ts
+++ web/angular2/basics/user-input/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { ClickMeComponent } from './click-me.component';
 import { KeyUpComponent } from './keyup.component'
 import { LoopbackComponent } from './loop-back.component';
+import { LittleTourComponent } from './little-tour.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-    directives: [ClickMeComponent, KeyUpComponent, LoopbackComponent]
+    directives: [ClickMeComponent, KeyUpComponent, LoopbackComponent, LittleTourComponent]
 })
 export class AppComponent {
   title = 'User Input';
