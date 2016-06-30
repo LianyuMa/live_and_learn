fb76f3bb551a4bbc3dde2adb97e1e85f4fe7d465
LianyuMa <LianyuMa@users.noreply.github.com>
2016-06-30 12:41:52 -0400

    Key event filtering

--- web/angular2/basics/user-input/app/keyup.component.ts
+++ web/angular2/basics/user-input/app/keyup.component.ts
@@ -6,10 +6,20 @@ import { Component } from '@angular/core';
     <h2>Give me some keys!</h2>
     <input #box (keyup)="onKey(box.value)">
     <p>{{values}}</p>
+    <h2>Type away! Press [enter] when done</h2>
+    <input #box1 (keyup.enter)="values1=box1.value">
+    <p>{{values1}}</p>
+    <h2>Type away! Press [enter] or click elsewhere when done.</h2>
+    <input #box2
+      (keyup.enter)="values2=box2.value"
+      (blur)="values2=box2.value">
+    <p>{{values2}}</p>
   `
 })
 export class KeyUpComponent {
   values = '';
+  values1 = '';
+  values2 = '';
 
   onKey(value:string) {
     this.values += value + ' |';
