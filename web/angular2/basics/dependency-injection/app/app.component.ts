55a9ba7a33b188c6ba8a3151a1c7d6e495081926
root <LianyuMa@users.noreply.github.com>
2016-07-02 18:30:22 -0400

    The Tour of Heroes Revision for Seprate Concern

--- web/angular2/basics/dependency-injection/app/app.component.ts
+++ web/angular2/basics/dependency-injection/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
 import { CarComponent } from './car/car.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
     selector: 'my-app',
     template: `
       <h1>Dependency Injection</h1>
       <my-car></my-car>
+      <my-heroes></my-heroes>
     `,
-    directives: [CarComponent]
+    directives: [CarComponent, HeroesComponent]
 })
 export class AppComponent { }
--- web/angular2/basics/dependency-injection/app/main.ts
+++ web/angular2/basics/dependency-injection/app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
+import {HeroService} from './heroes/hero.service';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [HeroService]);
