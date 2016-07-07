5594deac99cd2fd1e0b4a3ace2fffd8bebb46b04
root <LianyuMa@users.noreply.github.com>
2016-07-07 17:25:45 -0400

    add without pipe to command update the option

--- web/node/autogo/bin/autogo.js
+++ web/node/autogo/bin/autogo.js
@@ -8,12 +8,15 @@ var wf = require('../lib/withoutFile');
 program
   .version(require('../package.json').version)
   .usage('[options] [project name]')
+  .option('-W, --without <str | array>', 'generate project without some models(value can be `sass`, `coffee`, `jade`)')
   .parse(process.argv);
 
 var pname = program.args[0]
 if (!pname) program.help();
 
+var outs = program.without ? program.without.split(',') : [];
+
 Promise.all([gs(pname)])
   .then(function() {
-    return wf(pname, ['jade', 'sass']);
+    return wf(pname, outs);
   });
