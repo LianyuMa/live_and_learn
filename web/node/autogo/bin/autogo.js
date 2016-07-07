a93dfbba9e5d8eefe5545eeb8ae475a39126f1f7
root <LianyuMa@users.noreply.github.com>
2016-07-06 22:52:33 -0400

    add parameter config with commander

--- web/node/autogo/bin/autogo.js
+++ web/node/autogo/bin/autogo.js
@@ -1,6 +1,13 @@
 #! /usr/bin/env node
 
-// console.log("Hello")
+var program = require('commander');
 var gs = require('../lib/generateStructure');
 
-gs("demo");
+program
+  .version(require('../package.json').version)
+  .usage('[options] [project name]')
+  .parse(process.argv);
+
+var pname = program.args[0]
+
+gs(pname);
