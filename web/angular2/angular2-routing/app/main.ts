d074bd61d45c763df221f8a800417f9d21f03333
root <LianyuMa@users.noreply.github.com>
2016-07-09 23:07:28 -0400

    DI provideRouter(routes: RouteConfig)

--- web/angular2/angular2-routing/app/main.ts
+++ web/angular2/angular2-routing/app/main.ts
@@ -1,5 +1,7 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactsAppRoutes } from './contacts.routes';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [provideRouter(ContactsAppRoutes)]);
