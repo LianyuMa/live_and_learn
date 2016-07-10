68165dac4221298a0b7b3af262e43be22588c4e9
root <LianyuMa@users.noreply.github.com>
2016-07-09 22:47:45 -0400

    add ContactsDetailComponent

--- web/angular2/angular2-routing/app/app.component.ts
+++ web/angular2/angular2-routing/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ContactsListComponent } from './contacts-list.component'
+import { ContactsListComponent } from './contacts-list.component';
+import { ContactsDetailComponent } from './contacts-detail.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Routing</h1><contacts-list></contacts-list>',
-    directives: [ContactsListComponent]
+    template: '<h1>Angular 2 Routing</h1><contacts-list></contacts-list><contacts-detail></contacts-detail>',
+    directives: [ContactsListComponent, ContactsDetailComponent]
 })
 export class AppComponent { }
---
+++ web/angular2/angular2-routing/app/contacts-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contacts-detail',
+  template: `
+    <h2>{{contact.name}}</h2>
+
+    <address>
+      <span>{{contact.street}}</span>
+      <span>{{contact.zip}}</span>
+      <span>{{contact.city}}</span>
+      <span>{{contact.counrty}}</span>
+    </address>
+  `
+})
+export class ContactsDetailComponent { }
