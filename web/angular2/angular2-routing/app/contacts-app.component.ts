1b86d0018f37965dd516d15f61986ba7faca79f8
root <LianyuMa@users.noreply.github.com>
2016-07-10 22:35:16 -0400

    ROUTER_DIRECTIVES to predefine a list of directives

--- web/angular2/angular2-routing/app/contacts-app.component.ts
+++ web/angular2/angular2-routing/app/contacts-app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   selector: 'contacts-app',
   template: `
     <h1>Contacts App</h1>
+    <router-outlet></router-outlet>
   `,
+  directives: [ROUTER_DIRECTIVES]
 })
 export class ContactsAppComponent {
-  
+
 }
