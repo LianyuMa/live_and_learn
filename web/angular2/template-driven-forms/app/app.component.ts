f61df195ab05df43eba9501b651a9e00005a451a
root <LianyuMa@users.noreply.github.com>
2016-07-13 21:56:44 -0400

    using Angular's local variable mechanism

--- web/angular2/template-driven-forms/app/app.component.ts
+++ web/angular2/template-driven-forms/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+// import { DraggableComponent } from './draggable.component';
 
 @Component({
     selector: 'my-app',
     template: `
       <h1>Template Driven Forms</h1>
-      <form>
+      <form #form="ngForm">
         <label>Firstname:</label>
         <input type="text">
 
@@ -22,6 +23,8 @@ import { Component } from '@angular/core';
 
         <button type="submit">Submit</button>
       </form>
-    `
+      <div draggable #myDraggable="draggable">I'm draggable!</div>
+    `,
+    // directives: [DraggableComponent]
 })
 export class AppComponent { }
