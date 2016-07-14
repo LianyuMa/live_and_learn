8227cd82044b2d9d8e716da6cb66aeb3c2028e78
root <LianyuMa@users.noreply.github.com>
2016-07-13 21:20:08 -0400

    Initiate

---
+++ web/angular2/template-driven-forms/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Template Driven Forms</h1>'
+})
+export class AppComponent { }
---
+++ web/angular2/template-driven-forms/app/main.ts
@@ -0,0 +1,6 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent. [disableDeprecatedForms(), provideForms()]);
