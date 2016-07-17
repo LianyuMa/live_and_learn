690376f848b6d8c17939d24962448df6a90d3a89
root <LianyuMa@users.noreply.github.com>
2016-07-17 17:52:11 -0400

    Styles inline in transitions

--- web/angular2/developer-guide/animations/app/hero-team-builder.component.ts
+++ web/angular2/developer-guide/animations/app/hero-team-builder.component.ts
@@ -2,29 +2,17 @@ import { Component } from '@angular/core';
 
 import { Heroes } from './hero.service';
 import { HeroListBasicComponent } from './hero-list-basic.component';
+import { HeroListInlineStylesComponent } from './hero-list-inline-styles.component';
 
 @Component({
+  moduleId: module.id,
   selector: 'hero-team-builder',
-  template: `
-    <div class="buttons">
-      <button [disabled]="!heroes.canAdd()" (click)="heroes.addInactive()">Add inactive hero</button>
-      <button [disabled]="!heroes.canAdd()" (click)="heroes.addActive()">Add active</button>
-      <button [disabled]="!heroes.canRemove()" (click)="heroes.remove()">Remove hero</button>
-    </div>
-    <div class="columns">
-      <div class="column">
-        <h4>Basic State</h4>
-        <p>Switch between active/inactive on click.</p>
-        <hero-list-basic [heroes]=heroes></hero-list-basic>
-      </div>
-    </div>
-  `,
-  styles: [`
-    .buttons {
-      text-align: center;
-    }
-  `],
-  directives: [HeroListBasicComponent],
+  templateUrl: 'hero-team-builder.component.html',
+  styleUrls: [`hero-team-builder.component.css`],
+  directives: [
+    HeroListBasicComponent,
+    HeroListInlineStylesComponent
+  ],
   providers: [Heroes]
 })
 export class HeroTeamBuilderComponent {
