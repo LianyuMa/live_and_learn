d36203ed90e35ee0f8564779e17a3cdaef069a2f
root <LianyuMa@users.noreply.github.com>
2016-07-26 17:16:40 -0400

    Initiate

--- web/angular2/angular-meteor/socially/client/main.ts
+++ web/angular2/angular-meteor/socially/client/main.ts
@@ -1,14 +1,13 @@
 import 'reflect-metadata';
-import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
-import { bootstrap } from 'angular2-meteor-auto-bootstrap';
-
-import template from './app.html';
-
+import { bootstrap } from '@angular/platform-browser-dynamic';
+ 
+import template from './main.html';
+ 
 @Component({
   selector: 'app',
   template
 })
 class Socially { }
-
+ 
 bootstrap(Socially);
