2bc777947f0081fbb54dba582ed9cd5ffc6910d6
root <mlyoct24@gmail.com>
2016-07-27 23:52:14 -0400

    update

--- web/angular2/ng2-bootstrap/angular2-quickstart/app/app.component.ts
+++ web/angular2/ng2-bootstrap/angular2-quickstart/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Ng2Bootstrap } from './ng2-bootstrap.component.html';
+import { Ng2Bootstrap } from './ng2-bootstrap.component';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1><ng2-bootstrap></ng2-bootstrap>',
-    directive: [Ng2Bootstrap]
+    directives: [Ng2Bootstrap]
 })
 export class AppComponent { }
