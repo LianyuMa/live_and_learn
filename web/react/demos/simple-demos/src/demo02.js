905e08ad5bd17366a2bfa263aa06d7abb2f25472
root <mlyoct24@gmail.com>
2016-07-31 23:39:43 -0400

    Use JavaScript in JSX

---
+++ web/react/demos/simple-demos/src/demo02.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const names = ['Alice', 'Emily', 'Kate'];
+
+class Demo02 extends Component {
+  render() {
+    return(
+      <div>
+      {
+        names.map((name) => <div>Hello, {name}!</div>)
+      }
+      </div>
+    )
+  }
+}
+
+export default Demo02;
--- web/react/demos/simple-demos/src/index.js
+++ web/react/demos/simple-demos/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Demo01 from './demo01'
+import Demo01 from './demo01';
+import Demo02 from './demo02';
 import './index.css';
 
 ReactDOM.render(
-  <div><App /><Demo01 /></div>,
+  <div>
+    <App />
+    <Demo01 />
+    <Demo02 />
+  </div>,
   document.getElementById('root')
 );
