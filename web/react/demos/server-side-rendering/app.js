1afbf615c3b97dba3e6649d47773aa9a26e73bd1
root <mlyoct24@gmail.com>
2016-08-05 21:13:14 -0400

    update devDependencies and .babelrc

--- web/react/demos/server-side-rendering/app.js
+++ web/react/demos/server-side-rendering/app.js
@@ -1,75 +1,37 @@
-Object.defineProperty(exports, '__esModule', {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-const _createClass = () => { function defineProperties(target, props) { for (let i = 0; i < props.length; i++) {
-    const descriptor = props[i];
-    descriptor.enumerable = descriptor.enumerable || false;
-    descriptor.configurable = true;
-    if ('value' in descriptor) descriptor.writable = true;
-    Object.defineProperty(target, descriptor.key, descriptor);
-  }}
-  return (Constructor, protoProps, staticProps) => {
-    if (protoProps) defineProperties(Constructor, staticProps);
-    return Constructor;
-  };
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-const _get = function get(_x, _x2, _x3) {
-  const _again = true;
-  _function: while (_again) {
-    const object = _x;
-    const property = _x2;
-    const receiver = _x3;
-    desc = parent = getter = undefined;
-    _again = false;
-    if (object === null) object = Function.prototype;
-    let desc = Object.getOwnPropertyDescriptor(object, property);
-    if (desc === undefined) {
-      let parent = Object.getPrototypeOf(Object);
-      if (parent === null) {
-        return undefined;
-      } else {
-        _x = parent;
-        _x2 = property;
-        _x3 = receiver;
-        _again = true;
-        continue _function;
-      }
-    } else if ('value' in desc) {
-      return desc.value;
-    } else {
-      let getter = desc.get;
-      if (getter === undefined) {
-        return undefined;
-      }
-      return getter.call(receiver);
-    }
-  }
-};
+var _react = require('react');
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+var _react2 = _interopRequireDefault(_react);
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-import _react from 'react';
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-const _react2 = _interopRequireDefault(_react);
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-const App = ((_React$Component) => {
+var App = function (_React$Component) {
   _inherits(App, _React$Component);
 
   function App(props) {
     _classCallCheck(this, App);
 
-    _get(Object.getPrototypeOf(App.prototype), 'constructor', this).call(this, props);
-    this.render = this.render.bind(this);
-    this.state = {
-      items: this.props.items,
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(App).call(this, props));
+
+    _this.render = _this.render.bind(_this);
+    _this.state = {
+      items: _this.props.items,
       disabled: true
     };
+    return _this;
   }
 
   _createClass(App, [{
@@ -89,19 +51,19 @@ const App = ((_React$Component) => {
   }, {
     key: 'render',
     value: function render() {
-      return _react2['default'].createElement(
+      return _react2.default.createElement(
         'div',
         null,
-        _react2['default'].createElement(
+        _react2.default.createElement(
           'button',
           { onClick: this.handleClick.bind(this), disabled: this.state.disabled },
           'Add Item'
         ),
-        _react2['default'].createElement(
+        _react2.default.createElement(
           'ul',
           null,
-          this.state.items.map((item) => {
-            return _react2['default'].createElement(
+          this.state.items.map(function (item) {
+            return _react2.default.createElement(
               'li',
               null,
               item
@@ -113,8 +75,6 @@ const App = ((_React$Component) => {
   }]);
 
   return App;
-})(_react2['default'].Component);
+}(_react2.default.Component);
 
-exports['default'] = App;
-;
-module.exports = exports['default'];
+exports.default = App;
--- web/react/demos/server-side-rendering/browser.js
+++ web/react/demos/server-side-rendering/browser.js
@@ -1,9 +1,17 @@
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default' : obj }; }
-import _react from 'react';
-const _react2 = _interopRequireDefault(_react);
-import _reactDom from 'react-dom';
-const _reactDom2 = _interopRequireDefault(_reactDom);
-import _app from './app';
-const _app2 = _interopRequireDefault(_app);
+'use strict';
 
-_reactDom2['default'].render(_react2['default'].createElement(_app2['default'], { items: window.APP_PROPS.items }), document.getElementById('content'));
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _app = require('./app');
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_reactDom2.default.render(_react2.default.createElement(_app2.default, { items: window.APP_PROPS.items }), document.getElementById('content'));
