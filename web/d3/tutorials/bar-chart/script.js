3ed0572aa06720e59aef150ef7e89e1a0cdbaf0e
LianyuMa <mlyoct24@gmail.com>
2016-09-06 20:41:02 -0700

    manipulate single element or many without substantially restructuring

--- web/d3/tutorials/bar-chart/script.js
+++ web/d3/tutorials/bar-chart/script.js
@@ -1,3 +1,7 @@
-var div = document.createElement("div");
-div.innerHTML = "Hello, world!";
-document.body.appendChild(div);
+// var div = document.createElement("div");
+// div.innerHTML = "Hello, world!";
+// document.body.appendChild(div);
+
+var body = d3.select("body");
+var div = body.append("div");
+div.html("Hello, world!");
