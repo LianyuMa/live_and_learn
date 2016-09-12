7e7cc128b69d5379de1349ee163c1119905c2119
LianyuMa <mlyoct24@gmail.com>
2016-09-11 20:48:05 -0700

    Restructure the code

--- web/d3/tutorials/bar-chart-II/script.js
+++ web/d3/tutorials/bar-chart-II/script.js
@@ -1,27 +1,37 @@
-var data = [4, 8, 15, 16, 23, 42];
+// var data = [4, 8, 15, 16, 23, 42];
 
 var width = 420,
     barHeight = 20;
 
 var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
+    // .domain([0, d3.max(data)])
     .range([0, width]);
 
 var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length);
+    .attr("width", width);
+    // .attr("height", barHeight * data.length);
+d3.tsv("data.tsv", type, function(error, data) {
+  x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-var bar = chart.selectAll("g")
-    .data(data)
-  .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  chart.attr("height", barHeight * data.length);
 
-bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+  bar.append("rect")
+      .attr("width", x)
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
+});
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
