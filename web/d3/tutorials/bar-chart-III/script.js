1754796588a30c9f94bf5876a7c54cdd2fb6fae0
LianyuMa <mlyoct24@gmail.com>
2016-09-12 17:51:03 -0700

    Encoding Ordinal Data

--- web/d3/tutorials/bar-chart-III/script.js
+++ web/d3/tutorials/bar-chart-III/script.js
@@ -7,3 +7,33 @@ var y = d3.scale.linear()
 var chart = d3.select(".chart")
     .attr("width", width)
     .attr("height", height);
+
+var xAxis = d3.svg.linear()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var chart = d3.select(".chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.name; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+});
+
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
