18cbc2b20d02e3d0464f6e778202b3f963ab00fd
LianyuMa <mlyoct24@gmail.com>
2016-09-12 17:03:04 -0700

    Bar chart III

---
+++ web/d3/tutorials/bar-chart-III/script.js
@@ -0,0 +1,9 @@
+var width = 960,
+    height = 500;
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", height);
