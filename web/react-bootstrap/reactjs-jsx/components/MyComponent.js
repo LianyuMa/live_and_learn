4ed3cae1c5c22af0733278c3ccbc04a640471c57
LianyuMa <mlyoct24@gmail.com>
2017-03-02 13:55:18 -0500

    Toggle Example

---
+++ web/react-bootstrap/reactjs-jsx/components/MyComponent.js
@@ -0,0 +1,45 @@
+/* React.DOM */
+var MessagePanel = React.createClass({
+  render: function () {
+    return <div className='collapse in'>{this.props.children}</div>
+  }
+});
+
+MessagePanel.Heading = React.createClass({
+  render: function () {
+    return <h2>{this.props.text}</h2>
+  }
+});
+
+MessagePanel.Content = React.createClass({
+  render: function () {
+    return <div className='well'>{this.props.children}</div>
+  }
+});
+
+
+var MyApp = React.createClass({
+  getInitialState: function () {
+    return {
+      collapse: false
+    };
+  },
+
+  handleToggle: function() {
+    var nextState = this.state.collapse ?
+      (<MessagePanel>
+        <MessagePanel.Heading text='Show/Hide' />
+        <MessagePanel.Content>
+          Phasellus sed velit venenatils, suscipit eros a, laoreet dui.
+        </MessagePanel.Content>
+      </MessagePanel>)
+      : null;
+      return (<div>
+        <h1>Namespaced Components Demo</h1>
+        <p><button onClick={this.handleToggle} className="btn btn-primary">Toggle</button></p>
+        {showhideToggle}
+      </div>)
+  }
+});
+
+React.render(<MyApp/>, document.getElementById('toggle-example'));
