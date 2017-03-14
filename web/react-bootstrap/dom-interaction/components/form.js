3687bfce3bbb25ce2c51805e1a743c984be0b73b
Alex <mlyoct24@gmail.com>
2017-03-14 12:53:41 -0400

    get values for components

--- web/react-bootstrap/dom-interaction/components/form.js
+++ web/react-bootstrap/dom-interaction/components/form.js
@@ -4,17 +4,21 @@ var max_Char = '140';
 var AddTicket = React.createClass({
   handleSubmitEvent: function (event) {
     event.preventDefault();
+    console.log("Email--"+this.refs.email.value.trim());
+    console.log("Issue Type--"+this.refs.issueType.value.trim());
+    console.log("Department--"+this.refs.department.value.trim());
+    console.log("Comments--"+this.refs.comment.value.trim());
   },
   render: function() {
     return (
       <form onSubmit={this.handleSubmitEvent}>
         <div className="form-group">
           <label htmlFor="email">Email <span style={style}>*</span></label>
-          <input type="text" id="email" className="form-control" placeholder="Enter email" required />
+          <input type="text" id="email" className="form-control" placeholder="Enter email" required ref="email"/>
         </div>
         <div className="form-group">
           <label htmlFor="issueType">Issue Type <span style={style}>*</span></label>
-          <select className="form-control" id="issueType" required>
+          <select className="form-control" id="issueType" required ref="issueType">
             <option value="">-----Select-----</option>
             <option value="Access Related Issue">Access Related Issue</option>
             <option value="Email Related Issues">Email Related Issues</option>
@@ -27,7 +31,7 @@ var AddTicket = React.createClass({
         </div>
         <div className="form-group">
           <label htmlFor="department">Assign Department <span style={style}>*</span></label>
-          <select className="form-control" id="department" required>
+          <select className="form-control" id="department" required ref="department">
             <option value="">-----Select-----</option>
             <option value="Admin">Admin</option>
             <option value="HR">HR</option>
@@ -37,7 +41,7 @@ var AddTicket = React.createClass({
         </div>
         <div className="form-group">
           <label htmlFor="comments">Comments <span style={style}>*</span></label>(<span id="maxlength">200</span> characters left)
-          <textarea className="form-control" rows="3" id="comments" required></textarea>
+          <textarea className="form-control" rows="3" id="comments" required ref="comment"></textarea>
         </div>
         <div className="btn-group">
           <button type="submit" className="btn btn-primary">Submit</button>
